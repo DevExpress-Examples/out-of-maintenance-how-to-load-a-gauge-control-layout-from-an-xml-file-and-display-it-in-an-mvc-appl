b94253fea2094fb53dd3b21dcb2a55dd4d6feb69
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# How to load a gauge control layout from an XML file and display it in an MVC application
+
+
+<p>The example demonstrates how to load the ASPxGaugeControl layout (created in the WebForms application) and then render an appropriate image in the MVC application.</p><p>The sample is based on the blog post where Charts are created in a similar manner: <a href="http://community.devexpress.com/blogs/bryan/archive/2010/12/10/charts-in-asp-net-mvc.aspx"><u>Charts In ASP.NET MVC</u></a></p><p><strong>See also:</strong><strong><br />
+</strong><a href="https://www.devexpress.com/Support/Center/p/S35090">Implement the MVC extension for the ASPxGaugeControl</a></p>
+
+<br/>
+
+
---
+++ VB/DXWebApplication1/Controllers/HomeController.vb
@@ -0,0 +1,30 @@
+﻿Imports System
+Imports System.Drawing.Imaging
+Imports System.IO
+Imports System.Web.Mvc
+Imports DevExpress.Web.ASPxGauges
+Imports DevExpress.Web.ASPxGauges.Gauges.Circular
+
+Namespace DXWebApplication1.Controllers
+	Public Class HomeController
+		Inherits Controller
+
+		Public Function Index() As ActionResult
+			Return View()
+		End Function
+
+		<OutputCache(Duration := 1000, VaryByParam := "param")>
+		Public Function RenderGauge(ByVal param As Single) As ActionResult
+			Dim ctrl As New ASPxGaugeControl()
+
+			ctrl.RestoreLayoutFromXml(Server.MapPath("~/App_Data/gauge.xml"))
+			TryCast(ctrl.Gauges("myGauge"), CircularGauge).Scales("myScale").Value = param
+
+			Dim stream As New MemoryStream()
+			ctrl.ExportToImage(stream, ImageFormat.Png)
+
+			stream.Seek(0, SeekOrigin.Begin)
+			Return File(stream, "image/png")
+		End Function
+	End Class
+End Namespace
